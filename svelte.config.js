2576d2947ecd4a42d4011e813142c3f2e5b70bbc
taaheer <taaheerlabbe@gmail.com>
2026-02-18 20:01:21 +0530

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = { 
+    kit: { 
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: null
+        }),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/music' : ''
+        }
+    } };
+
+export default config;
