02dc4fa54fdafdc0c35b18fc4cd9448381d90710
taaheer <taaheerlabbe@gmail.com>
2026-02-18 20:34:39 +0530

    test 4

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,11 @@ import adapter from '@sveltejs/adapter-static';
 const config = { 
     kit: { 
         adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: null
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true        
         }),
         paths: {
             base: process.argv.includes('dev') ? '' : '/music'        
