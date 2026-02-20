b6816256f4a61b8c6e52b0c5f9d3c88b5a0e5b1a
taaheer <taaheerlabbe@gmail.com>
2026-02-20 16:40:02 +0530

    git

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,5 +4,5 @@ This is restricted area, you are not suppposed to here.
 
 Well you not gonna find anything here other than this text.
 
-Here the link to go back at start <a href="taaheer.github.io">Click</a>
+Here the link to go back at start <a href="https://taaheer.github.io">Click</a>
 
